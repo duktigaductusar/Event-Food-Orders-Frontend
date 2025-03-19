8d801bda3ba090d0cf5494d5692adbe16b77a221
Angeliki Vlachou <angeliki.vlachou@ductus.se>
2025-03-19 10:45:50 +0100

    Super refinement event components and templates

--- src/app/components/html/generic-btn/generic-btn.component.ts
+++ src/app/components/html/generic-btn/generic-btn.component.ts
@@ -1,32 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-export type GenericBtnCompClrType = (
-  "secondary" | "primary" | "danger" | "warning"
-)
+export type GenericBtnCompClrType =
+	| "secondary"
+	| "primary"
+	| "danger"
+	| "warning";
 
 @Component({
-  selector: 'app-generic-btn',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <button 
-      class="btn btn-sm w-100"
-      [ngClass]="'btn-outline-' + borderColor"
-      (click)="onClick($event)"
-    >
-      <i [class]="'bi bi-' + icon"></i> {{ label }}
-    </button>
-  `
+	selector: "app-generic-btn",
+	standalone: true,
+	imports: [CommonModule],
+	template: `
+		<button
+			class="btn btn-sm w-100"
+			[ngClass]="'btn-outline-' + borderColor"
+			(click)="onClick($event)"
+		>
+			<i [class]="'bi bi-' + icon"></i> {{ label }}
+		</button>
+	`,
 })
 export class GenericBtnComponent {
-  @Input() borderColor: GenericBtnCompClrType = 'secondary';
-  @Input() label = 'Click Me';
-  @Input() icon = 'pencil';
+	@Input() borderColor: GenericBtnCompClrType = "secondary";
+	@Input() label = "Click Me";
+	@Input() icon = "pencil";
 
-  @Output() action = new EventEmitter<Event>();
+	@Output() action = new EventEmitter<Event>();
 
-  onClick(event: Event) {
-    this.action.emit(event);
-  }
+	onClick(event: Event) {
+		this.action.emit(event);
+	}
 }
--- src/app/components/shared/datetimelabel/datetimelabel.component.ts
+++ src/app/components/shared/datetimelabel/datetimelabel.component.ts
@@ -1,19 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-datetimelabel',
-  imports: [CommonModule],
-  templateUrl: './datetimelabel.component.html',
-  styleUrl: './datetimelabel.component.css'
+	selector: "app-datetimelabel",
+	imports: [CommonModule],
+	templateUrl: "./datetimelabel.component.html",
+	styleUrl: "./datetimelabel.component.css",
 })
-export class DatetimelabelComponent implements OnInit {  
-  @Input() datevalue!: Date;
+export class DatetimelabelComponent implements OnInit {
+	@Input() datevalue!: Date;
 
-  ngOnInit() {
-    if (this.datevalue === undefined) {
-      throw new Error(`The 'datevalue' attribute is required for <app-datetimelabel>`);
-    }
-  }
-
+	ngOnInit() {
+		if (this.datevalue === undefined) {
+			throw new Error(
+				`The 'datevalue' attribute is required for <app-datetimelabel>`
+			);
+		}
+	}
 }
