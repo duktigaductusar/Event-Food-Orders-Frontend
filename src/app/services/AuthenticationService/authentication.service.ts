90ee20f6e2036f45044a5c95f08df53fb138fe28
rikrhen <riki.rhen@ductus.se>
2025-03-20 09:50:20 +0100

    Login page working and properly redirects to backend

--- src/app/services/AuthenticationService/authentication.service.ts
+++ src/app/services/AuthenticationService/authentication.service.ts
@@ -1,21 +1,18 @@
 import { HttpClient } from "@angular/common/http";
-import { Router } from "@angular/router";
 import { Injectable } from "@angular/core"
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthenticationService{
-    private apiUrl = "http://localhost:3000/api/auth";
+    private apiUrl = "http://localhost:8080/api/Authorization";
     
-    constructor(private http: HttpClient, private router: Router) {}
-    login(): void {
-        window.location.href = `${this.apiUrl}/login`;
+    constructor(private http: HttpClient) {
+        console.log("AuthService initialized");
     }
 
-    //Probably wont work and probably never gonna be used?
-    getAuthStatus(): Promise<any> {
-        return this.http.get(`${this.apiUrl}/callback`, {withCredentials: true}).toPromise();
+    login(): void {
+        window.location.href = `${this.apiUrl}/login`;
     }
 
     logout(): void {
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,16 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
+import { provideHttpClient } from "@angular/common/http";
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+const updatedAppConfig = {
+    ...appConfig,
+    providers: [
+        ...(appConfig.providers || []),
+        provideHttpClient()
+    ]
+};
+
+
+
+bootstrapApplication(AppComponent, updatedAppConfig).catch(err => console.error(err));
