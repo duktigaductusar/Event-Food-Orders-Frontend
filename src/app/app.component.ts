86dd55ca64a3e529d7faddcdcda470217e8d3a01
Joel Martelleur <joel.martelleur@gmail.com>
2025-03-26 05:46:33 +0100

    Update create event form UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from "@angular/core";
-import {
-	Router,
-	NavigationEnd,
-	RouterOutlet,
-	NavigationStart,
-	NavigationCancel,
-	NavigationError,
-} from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
 	selector: "app-root",
@@ -17,22 +10,4 @@ import {
 })
 export class AppComponent {
 	title = "dd-frontend";
-
-	constructor(private router: Router) {
-		// USED TO DEBUG ISSUE WITH ROUTING.
-		this.router.events.subscribe(event => {
-			if (event instanceof NavigationStart) {
-				console.log("	NavigationStart:", event.url);
-			}
-			if (event instanceof NavigationEnd) {
-				console.log("NavigationEnd:", event.urlAfterRedirects);
-			}
-			if (event instanceof NavigationCancel) {
-				console.warn("NavigationCancel:", event.reason);
-			}
-			if (event instanceof NavigationError) {
-				console.error("NavigationError:", event.error);
-			}
-		});
-	}
 }
