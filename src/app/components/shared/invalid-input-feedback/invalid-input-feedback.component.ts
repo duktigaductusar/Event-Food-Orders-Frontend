62cfb2585ab8974f2d56879388448508f326f470
Joel Martelleur <joel.martelleur@gmail.com>
2025-03-30 12:22:14 +0200

    Create reusable components for invalid input feedback and required label.

--- src/app/components/shared/invalid-input-feedback/invalid-input-feedback.component.ts
+++ src/app/components/shared/invalid-input-feedback/invalid-input-feedback.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-invalid-input-feedback',
@@ -6,9 +6,7 @@ import { Component, input, OnInit } from '@angular/core';
   templateUrl: './invalid-input-feedback.component.html',
   styleUrl: './invalid-input-feedback.component.css'
 })
-export class InvalidInputFeedbackComponent implements OnInit {
-  ngOnInit(): void {
-    console.log("text", this.text())
-  }
+export class InvalidInputFeedbackComponent {
+  show = input(false);
   text = input("invalid input")
 }
