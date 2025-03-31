84d64dc5352abf3c9576053aebd09dbba6414a9d
danmicha <daniel.michanech@ductus.se>
2025-03-31 14:15:53 +0200

    Event-management gets data from event-list card.

--- src/app/components/pages/event-management-page/event-management-page.component.ts
+++ src/app/components/pages/event-management-page/event-management-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { EventManagementFormComponent } from "../../features/event-management-form/event-management-form.component";
+import { MainLayoutComponent } from '@app/components/layouts';
 
 @Component({
   selector: 'app-event-management-page',
-  imports: [],
+  imports: [MainLayoutComponent, EventManagementFormComponent],
   templateUrl: './event-management-page.component.html',
   styleUrl: './event-management-page.component.css'
 })
