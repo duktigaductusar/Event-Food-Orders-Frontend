fba98e85d1ba4a7bf46b8160942e34791962d70d
Angeliki Vlachou <angeliki.vlachou@ductus.se>
2025-03-31 16:43:14 +0200

    Connect event response with be

--- src/app/components/features/events/event-item/event-item.component.ts
+++ src/app/components/features/events/event-item/event-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
-import type { IEventDto } from "@app/models";
+import type { IEventDto, IParticipantForUpdateDto } from "@app/models";
 import { AppBaseComponent } from "@app/components/base/app-base.component";
 import { DatetimelabelComponent } from "@app/components/shared/datetimelabel/datetimelabel.component";
 import { GenericBtnComponent } from "../../../html/generic-btn/generic-btn.component";
@@ -7,6 +7,7 @@ import { Router } from "@angular/router";
 import { appRoutes } from "@app/constants";
 import { EventService } from "@app/services";
 import { StatusLabelComponent } from "../../../shared";
+import type { ParticipantResponseType } from "@types";
 
 @Component({
 	selector: "app-event-item",
@@ -22,10 +23,12 @@ import { StatusLabelComponent } from "../../../shared";
 export class EventItemComponent extends AppBaseComponent {
 	@Input() eventDto!: IEventDto;
 	@Output() cardClick = new EventEmitter<void>();
-	@Output() actionClick = new EventEmitter<{
-		action: string;
-		eventDto: IEventDto;
-	}>();
+	// @Output() actionClick = new EventEmitter<{
+	// 	action: string;
+	// 	eventDto: IEventDto;
+	// }>();
+
+	participantResponseTypes : ParticipantResponseType[] = ["PENDING", "ATTENDING_ONLINE", "ATTENDING_OFFICE", "NOT_ATTENDING"]
 
 	constructor(
 		private router: Router,
@@ -34,9 +37,33 @@ export class EventItemComponent extends AppBaseComponent {
 		super();
 	}
 
-	onAction(event: Event, action: string): void {
+	onAction(event: Event, action: ParticipantResponseType): void {
 		event.stopPropagation(); // Prevent card click event
-		this.actionClick.emit({ action, eventDto: this.eventDto });
+		// this.actionClick.emit({ action, eventDto: this.eventDto });
+		const Dto:IParticipantForUpdateDto= {
+			responseType: "PENDING",
+			wantsMeal: false,
+			allergies: '',
+			preferences: ''
+		}
+		//todo need to fetch participant id before updating the response
+		// switch (action) {
+		// 	case "PENDING":
+		// 		return "Waiting for response...";
+		// 	case "ATTENDING_ONLINE":
+		// 		return "Participant is joining online.";
+		// 	case "ATTENDING_OFFICE":
+		// 		return "Participant is attending in the office.";
+		// 	case "NOT_ATTENDING":
+		// 		return "Participant is not attending.";
+		// 	default:
+		// 		return "Unknown response.";
+		// }
+	}
+
+	onEdit(event: Event): void {
+		event.stopPropagation();
+		//this.actionClick.
 	}
 
 	selectedEvent() {
