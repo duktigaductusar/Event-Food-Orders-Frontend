8daedaaaa76ba18684b4a40a7b0b22d1e3425edf
Joel Martelleur <joel.martelleur@gmail.com>
2025-04-02 09:36:11 +0200

    Setup global error handling

--- src/app/components/html/error-modal/error-modal.component.ts
+++ src/app/components/html/error-modal/error-modal.component.ts
@@ -1,26 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { GenericBtnComponent } from "../generic-btn/generic-btn.component";
 
 @Component({
 	selector: "app-error-modal",
 	standalone: true,
-	template: `
-		<div class="modal-header bg-danger text-white">
-			<h5 class="modal-title">{{ title }}</h5>
-			<button
-				type="button"
-				class="btn-close"
-				aria-label="Close"
-				(click)="close()"
-			></button>
-		</div>
-		<div class="modal-body">
-			<p>{{ message }}</p>
-			<button class="btn btn-outline-secondary mt-2" (click)="close()">
-				Close
-			</button>
-		</div>
-	`,
+	templateUrl: "./error-modal.component.html",
+	imports: [GenericBtnComponent],
 })
 export class ErrorModalComponent {
 	@Input() title = "";
