549005e67724b1424eb00b3e9016abaaa8b9bdea
Joel Martelleur <joel.martelleur@ductus.se>
2025-04-08 16:42:21 +0200

    Clean up env files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,7 +32,7 @@ export const routes: Routes = [
 			},
 		],
 	},
-	{ path: appRoutes.LOGOUT, component: LogoutPageComponent },	
+	{ path: appRoutes.LOGOUT, component: LogoutPageComponent },
 	{
 		path: appRoutes.LOGOUT,
 		component: LogoutPageComponent,
