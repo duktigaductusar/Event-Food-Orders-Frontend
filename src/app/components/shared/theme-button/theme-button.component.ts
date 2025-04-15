de1ff6bd87b277c4d95068da871027fd91d4ac0a
Joel Martelleur <joel.martelleur@gmail.com>
2025-04-15 10:08:16 +0200

    Update themes for create, manage, and details pages.

--- src/app/components/shared/theme-button/theme-button.component.ts
+++ src/app/components/shared/theme-button/theme-button.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
+import { NgbPopoverModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { ButtonWrapperComponent } from "@app/components/html";
 import { ThemeService } from "@app/services";
 
 @Component({
 	selector: "app-theme-button",
-	imports: [CommonModule, ButtonWrapperComponent],
+	imports: [CommonModule, ButtonWrapperComponent, NgbPopoverModule],
 	templateUrl: "./theme-button.component.html",
 	styleUrl: "./theme-button.component.css",
 })
 export class ThemeButtonComponent {
+	title = input("Toggle dark and light themes");
+
 	constructor(public readonly themeService: ThemeService) {}
 
 	toggle() {
