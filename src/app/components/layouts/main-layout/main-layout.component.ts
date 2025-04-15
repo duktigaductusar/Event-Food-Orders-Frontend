28f4f35dc8f575bc874899ec364201d325154f3e
Joel Martelleur <joel.martelleur@gmail.com>
2025-04-15 10:33:25 +0200

    Update themes for create, manage, and details pages.

--- src/app/components/layouts/main-layout/main-layout.component.ts
+++ src/app/components/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectorRef, Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
 import { NgbOffcanvas } from "@ng-bootstrap/ng-bootstrap";
 
 import { appRoutes } from "@app/constants";
 import { AppBaseComponent } from "@app/components/base/app-base.component";
 import { ThemeButtonComponent } from "@app/components/shared";
 import { ButtonWrapperComponent } from "@app/components/html";
+import { Location } from "@angular/common";
 
 @Component({
 	selector: "app-main-layout",
@@ -18,8 +19,10 @@ export class MainLayoutComponent extends AppBaseComponent {
 	readonly appRoutes = appRoutes;
 
 	constructor(
-		private offcanvasService: NgbOffcanvas,
-		private cdRef: ChangeDetectorRef
+		private readonly offcanvasService: NgbOffcanvas,
+		private readonly cdRef: ChangeDetectorRef,
+		private readonly location: Location,
+		private readonly router: Router
 	) {
 		super();
 	}
@@ -31,5 +34,13 @@ export class MainLayoutComponent extends AppBaseComponent {
 	closeSidebar() {
 		this.offcanvasService.dismiss();
 		this.cdRef.detectChanges();
+	}
+
+	goBack() {
+		this.location.back();
+	}
+
+	isHomePage(): boolean {
+		return this.router.url === `${appRoutes.HOME}/`;
 	}
 }
