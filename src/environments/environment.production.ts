b4202cded28c60913f7e68baf6eb1e1748faa1cd
Joel Martelleur <joel.martelleur@gmail.com>
2025-04-20 21:22:04 +0200

    spike deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,13 +2,28 @@ export const environment = {
 	production: true,
 	allowedEmailDomains: ["ductus.se"],
 	defaultDeadline: 1,
-	apiUrl: "http://localhost:8080/api",
+	apiUrl: "https://api.duktiga-ductusar.online/api",
 	azureAd: {
 		clientId: "3049ab04-0e32-4c6b-99ed-020071d7b4ad",
 		apiId: "818188f3-6c98-4bed-8a82-74454065975b",
 		authority:
 			"https://login.microsoftonline.com/f84f6fe6-b0e4-4db1-b7d6-b7c280fb507f",
-		loginRedirectUri: "https://efo.lulea.ducteas.se/",
-		logoutRedirectUri: "http://efo.lulea.ducteas.se/logout-success",
+		loginRedirectUri: "https://app.duktiga-ductusar.online/",
+		logoutRedirectUri: "https://app.duktiga-ductusar.online/logout-success",
 	},
 };
+
+// export const environment = {
+// 	production: true,
+// 	allowedEmailDomains: ["ductus.se"],
+// 	defaultDeadline: 1,
+// 	apiUrl: "https://efobackend.lulea.ductus.se/api",
+// 	azureAd: {
+// 		clientId: "3049ab04-0e32-4c6b-99ed-020071d7b4ad",
+// 		apiId: "818188f3-6c98-4bed-8a82-74454065975b",
+// 		authority:
+// 			"https://login.microsoftonline.com/f84f6fe6-b0e4-4db1-b7d6-b7c280fb507f",
+// 		loginRedirectUri: "https://efo.lulea.ducteas.se/",
+// 		logoutRedirectUri: "http://efo.lulea.ducteas.se/logout-success",
+// 	},
+// };
